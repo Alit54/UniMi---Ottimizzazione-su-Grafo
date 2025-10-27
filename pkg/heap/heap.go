5e5d0a482a0d14cc8fc6dafdd863bc4092f505db
Simone Alessandro Casciaro <simonealex.casciaro@gmail.com>
2025-10-27 17:17:28 +0100

    Interfaccia heap

---
+++ pkg/heap/heap.go
@@ -0,0 +1,31 @@
+package heap
+
+type Heap interface {
+	/*
+		Inserisce un elemento nello heap.
+		Precondizione: non deve esistere nodeID già all'interno dello heap
+	*/
+	Insert(nodeID int, distance int)
+	/*
+		Estrae il nodo con distanza minima.
+		Precondizione: heap non deve essere vuoto
+	*/
+	ExtractMin() (nodeID int, distance float64)
+	/*
+		Diminusce il valore nello heap di un nodo esistente
+		Precondizione: la nuova distanza deve essere minore e il nodo deve esistere nello heap
+	*/
+	DecreaseKey(nodeID int, newDistance int)
+	/*
+		Stampa la struttura dello heap
+	*/
+	PrintHeap()
+	/*
+		Restituisce il numero di elementi dello heap
+	*/
+	Size() int
+	/*
+		Controlla che lo heap sia vuoto
+	*/
+	IsEmpty() bool
+}
