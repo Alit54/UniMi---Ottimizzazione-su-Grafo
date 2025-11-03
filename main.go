0c101515ce209eb9cfb8b4b28e9b06352a9fdf3e
Simone Alessandro Casciaro <simonealex.casciaro@gmail.com>
2025-11-03 11:15:05 +0100

    Main per test visualizzazione

--- main.go
+++ main.go
@@ -3,33 +3,27 @@ package main
 import (
 	"OttimizzazioneSuGrafo/internal/graph"
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("First method")
-	a := graph.CreateGraphSelectRandom(0.5, 4, true)
-	for i := 0; i < len(a); i++ {
-		fmt.Printf("%2d ", a[i])
+	g := graph.NewGraph(true)
+	n := 50
+	for i := 0; i < n; i++ {
+		g.AddNode(strconv.Itoa(i))
+	}
+	selected := graph.CreateGraphSelectSuitablyElements(0.1, n, true)
+	for i := 0; i < len(selected); i++ {
+		fmt.Println(selected[i])
+		g.AddEdge(selected[i], 1)
 	}
 
-	fmt.Println("\nSecond method")
-	b := graph.CreateGraphSelectRandomSubset(0.5, 4, true)
-	for i := 0; i < len(b); i++ {
-		fmt.Printf("%2d ", b[i])
+	// ------------
+	err := g.ExportToJSON("export/graph.json")
+	if err != nil {
+		fmt.Println("Errore export:", err)
+		return
 	}
-	fmt.Println("\nThird method")
-	c := graph.CreateGraphDiscardRepetition(0.5, 4, true)
-	for i := 0; i < len(c); i++ {
-		fmt.Printf("%2d ", c[i])
-	}
-	fmt.Println("\nFourth method")
-	d := graph.CreateGraphHashFunction(0.5, 4, true, 2)
-	for i := 0; i < len(d); i++ {
-		fmt.Printf("%2d ", d[i])
-	}
-	fmt.Println("\nFifth method")
-	e := graph.CreateGraphSelectSuitablyElements(0.5, 4, true)
-	for i := 0; i < len(e); i++ {
-		fmt.Printf("%2d ", e[i])
-	}
+
+	fmt.Println("Grafo esportato in graph.json")
 }
